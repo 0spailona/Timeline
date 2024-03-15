1c524d5c9c0a25c69312cbaeead7cf09f807c744
Daria <n-d-p@mail.ru>
2024-03-16 02:05:58 +0300

    it works

--- README.md
+++
@@ -1,1 +0,0 @@
-## CI [![Build status](https://ci.appveyor.com/api/projects/status/da13vhx7kc5e77mi?svg=true)](https://ci.appveyor.com/project/0spailona/timeline)
--- src/app.js
+++
@@ -1,5 +0,0 @@
-import Controller from "./Controller";
-
-const container = document.querySelector('#root')
-const controller = new Controller(container);
-controller.init()
