64bb59d0e8eaaff73c97ea9368a1e09e8b902840
Daria <n-d-p@mail.ru>
2024-03-16 01:53:12 +0300

    it works

--- src/Controller.js
+++ src/Controller.js
@@ -1,9 +1,10 @@
 import MessageView from "./MessageView";
 import dateFormat from "dateformat";
+import checkCoordsType from "./checkCoordsType";
 
 export default class Controller {
   constructor(container) {
-    this.container = container
+    this.container = container;
   }
 
   init() {
@@ -12,7 +13,6 @@ export default class Controller {
       return
     }
     this.bindToDOM()
-
     this.getCoords()
   }
 
@@ -30,7 +30,6 @@ export default class Controller {
     const created = new Date().toISOString();
     data.created = dateFormat(created, 'dd.mm.yy  HH:MM');
     data.coords = this.coordsView;
-    console.log(data)
     const msg = new MessageView(document.querySelector('.messageContainer'));
     msg.drawMessage(data);
     e.target.reset()
@@ -42,7 +41,7 @@ export default class Controller {
         this.coords = {latitude: data.coords.latitude, longitude: data.coords.longitude};
         this.coordsView = `[${data.coords.latitude}, ${data.coords.longitude}]`
       }, err => {
-        console.log('err', err);
+        console.log(err)
         this.showInputCoordsForm()
       })
     }
@@ -51,19 +50,39 @@ export default class Controller {
 
   showInputCoordsForm() {
     this.container.querySelector('.absoluteContainer').style.display = 'block';
-    const formEl = this.container.querySelector('.addGeolocationForm')
-    formEl.addEventListener('submit', this.getCoordsFromForm.bind(this));
-    formEl.addEventListener('reset',() => {
-      this.container.querySelector('.absoluteContainer').style.display = 'none';
-    })
+    const formEl = this.container.querySelector('.addGeolocationForm');
+    const inputEl = formEl.querySelector('.coordsInput');
+    inputEl.onchange = () => {
+      const {validate, msg} = checkCoordsType(inputEl.value);
+      console.log('validate', {validate, msg})
+      if (!validate) {
+        inputEl.setCustomValidity(msg)
+      }
+    }
 
+    formEl.addEventListener('submit', this.getCoordsFromForm.bind(this));
   }
+
 
   getCoordsFromForm(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
-    console.log('data', data)
+    this.coords = data;
+    this.coordsView = this.getCoordsView(data.coords);
+    e.target.reset();
+    this.container.querySelector('.absoluteContainer').style.display = 'none';
   }
 
+  getCoordsView(coords) {
+    coords.trim();
+    if (coords.startsWith('[')) coords = coords.slice(1, coords.length);
+    if (coords.endsWith(']')) coords = coords.slice(0, coords.length - 1);
+    const commasIndex = coords.indexOf(',');
+    if (coords[commasIndex + 1] !== ' ') {
+      coords = coords.replace(',', ', ')
+    }
+
+    return `[${coords}]`
+  }
 }
